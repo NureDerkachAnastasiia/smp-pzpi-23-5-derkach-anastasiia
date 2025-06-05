8c51d858a60f8fee181dd10937b736c3c58f6d94
Anastasiia Derkach <168988368+NureDerkachAnastasiia@users.noreply.github.com>
2025-06-05 15:22:38 +0200

    Update checkout.php

--- Lab3/smp-pzpi-23-5-derkach-anastasiia-lab3/checkout.php
+++ Lab3/smp-pzpi-23-5-derkach-anastasiia-lab3/checkout.php
@@ -5,11 +5,9 @@ $db = initDB(__DIR__ . '/db/shop.db');
 session_start();
 $sessionId = session_id();
 
-// Очищаем корзину
 $stmt = $db->prepare("DELETE FROM cart WHERE session_id = :sid");
 $stmt->bindValue(':sid', $sessionId, SQLITE3_TEXT);
 $stmt->execute();
 
-// Перенаправление на главную с параметром
 header("Location: index.php?paid=1");
 exit;
